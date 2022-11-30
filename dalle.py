29a3a97a14d5942e2313686e4b5c61912e716553
Khalid Elassaad <khalidelassaad@gmail.com>
2022-11-30 13:11:01 -0800

    code: save outputs to a file in repo/outputs

--- dalle.py
+++ dalle.py
@@ -2,6 +2,8 @@ import os
 import openai
 import sys
 import webbrowser
+import time
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,3 +24,11 @@ if __name__ == "__main__":
     prompt = " ".join(sys.argv[1:])
     imageURL = getImageUrl(prompt)
     webbrowser.open(imageURL)
+
+    filename = prompt+"_"+str(int(time.time()))+".png"
+    filepath = "/Users/khalidelassaad/Desktop/khaliddalle/outputs/"+filename
+    response = requests.get(imageURL)
+    if response.status_code:
+        fp = open(filepath, 'wb')
+        fp.write(response.content)
+        fp.close()

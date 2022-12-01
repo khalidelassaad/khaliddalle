de264f708844fbbcda90d86cb44113cdd4a0342a
Khalid Elassaad <khalidelassaad@gmail.com>
2022-12-01 10:27:12 -0800

    code: save image generators in dict

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import dalle
+import sd2
 import sys
 
 from saveutil import openUrlAndSavePhoto
 
 
-getUrlFromPrompt = dalle.getUrlFromPrompt
-# getUrlFromPrompt = sd2.getUrlFromPrompt
+urlFromPromptDict = {
+    "dalle": dalle.getUrlFromPrompt,
+    "sd2": sd2.getUrlFromPrompt
+}
 
 if __name__ == "__main__":
     try:
         prompt = " ".join(sys.argv[1:])
-        imageURL = getUrlFromPrompt(prompt)
+        imageURL = urlFromPromptDict["sd2"](prompt)
         openUrlAndSavePhoto(imageURL, prompt)
     except Exception as e:
         print(e)

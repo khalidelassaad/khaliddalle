f4bfca9bf4e3324d032a6243bf6397836428bb6b
Khalid Elassaad <khalidelassaad@gmail.com>
2022-11-30 21:23:14 -0800

    refactor: short errors

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ getUrlFromPrompt = dalle.getUrlFromPrompt
 # getUrlFromPrompt = sd2.getUrlFromPrompt
 
 if __name__ == "__main__":
+    try:
         prompt = " ".join(sys.argv[1:])
         imageURL = getUrlFromPrompt(prompt)
         openUrlAndSavePhoto(imageURL, prompt)
+    except Exception as e:
+        print(e)

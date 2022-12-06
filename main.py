3337ba5139208a2abb6bb5945b6f11d867d6b4be
Khalid Elassaad <khalidelassaad@gmail.com>
2022-12-06 14:19:08 -0800

    code: add code for parsing count from command line

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import dalle
 import sd2
 import sys
+import argparse
 
 from saveutil import openUrlAndSavePhoto
 
+CURRENT_MODEL = "dalle"
+
+parser = argparse.ArgumentParser(
+    prog='Dalle',
+    description='Send a prompt to DallE or other AI Art API',
+    epilog='Current Model: ' + CURRENT_MODEL)
+parser.add_argument('prompt', nargs="+")
+parser.add_argument('-n', '--number', default=1)
+args = parser.parse_args()
+
 
 urlFromPromptDict = {
-    "dalle": dalle.getUrlFromPrompt,
-    "sd2": sd2.getUrlFromPrompt
+    "dalle": dalle.getUrlFromPrompt,  # TODO: add optional index and handle count
+    "sd2": sd2.getUrlFromPrompt  # TODO: add optional index and handle count
 }
 
 if __name__ == "__main__":
+    prompt = " ".join(args.prompt)
+    count = args.number
     try:
-        prompt = " ".join(sys.argv[1:])
-        imageURL = urlFromPromptDict["dalle"](prompt)
-        openUrlAndSavePhoto(imageURL, prompt)
+        imageURLs = urlFromPromptDict[CURRENT_MODEL](prompt, count)
+        for imageURL, index in enumerate(imageURLs):
+            openUrlAndSavePhoto(imageURL, prompt, index)
     except Exception as e:
         print(e)
